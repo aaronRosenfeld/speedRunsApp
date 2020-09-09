9e7b610d7f1639357e5750f5243e01d9def9863a
aaronRosenfeld <aaron.rosenfeld.1@gmail.com>
2020-09-09 00:28:04 -0400

    initial project

---
+++ speedRuns/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  speedRuns
+//
+//  Created by Aaron Rosenfeld on 9/9/20.
+//  Copyright © 2020 Aaron Rosenfeld. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
